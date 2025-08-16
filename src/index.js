899606c1447949a0c1c40cf84b4603d4f99d7796
midnightdream-918 <ngoclinh1605.0505@gmail.com>
2025-08-16 21:49:53 +0700

    Giai cuu ban toi

--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ import Body from './components/Body';
 import Footer from './components/Footer';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-
-import(BrowserRouter)
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
